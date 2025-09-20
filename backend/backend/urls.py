37f7028647726a13c4438215b76cfd547099e777
BhavyaProjects07 <laptopuse01824x@gmail.com>
2025-09-20 15:13:45 +0530

    added some missing packages

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('ai_chat.urls')),
+    
     path("auth/", include("users.urls")),
     path("documents/", include("documents.urls"))
 ]
